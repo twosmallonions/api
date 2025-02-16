cfbd7435470a53d43fd1f6e65c78f6ff8defdb9a
Marius Meschter <git@meschter.me>
2025-02-17 00:14:44 +0100

    remove drizzle

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,6 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
 import { Kysely, PostgresDialect } from 'kysely';
 import { Pool } from 'pg';
 import { DB } from './types';
-
-console.log(process.env['TSO_DB_URL']);
-export const db = drizzle(process.env['TSO_DB_URL'] ?? '');
 
 export function getDb(uri: string) {
     const db = new Kysely<DB>({
